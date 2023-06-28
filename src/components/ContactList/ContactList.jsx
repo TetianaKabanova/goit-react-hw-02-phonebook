001cd8d655f0ef3edce2ac50b5abb4fae2158fae
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-06-28 14:08:30 +0200

    added components

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ListWrap, List, Paragraf } from './ContactList.styled';
+import { Button } from 'components/FormList/FormList.styled';
+import { UserDeleteOutlined } from '@ant-design/icons';
+
+const ContactList = ({ contacts, onDelete }) => {
+  return (
+    <ListWrap>
+      {contacts.map(({ id, name, number }) => (
+        <List key={id}>
+          <Paragraf>{name}</Paragraf>
+          <Paragraf>{number}</Paragraf>
+          <Button type="button" onClick={() => onDelete(id)}>
+            <UserDeleteOutlined />
+          </Button>
+        </List>
+      ))}
+    </ListWrap>
+  );
+};
+
+export default ContactList;
